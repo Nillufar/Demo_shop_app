d4b68bdd649d1ef35f8344a00898fbcb13fbba83
Nilufar Kurbonova <kurbonovanilufar178@gmail.com>
2024-09-15 13:10:20 +0900

    Initial Commit

---
+++ Cloth shop/Cloth_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloth_shopApp.swift
+//  Cloth shop
+//
+//  Created by Nilufar Kurbonova on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Cloth_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
